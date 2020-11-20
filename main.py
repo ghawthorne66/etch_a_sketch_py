338ea06c6ea6a18782e009473e9961c55cd06628
Greg Hawthorne <ghawthorne@me.com>
2020-11-20 06:50:28 +0100

    add event listeners

--- main.py
+++ main.py
@@ -8,6 +8,44 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+
+
 screen.listen()
+screen.onkey(key="Up", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="Down", fun=move_backwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="Left", fun=turn_left)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="Right", fun=turn_right)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="Escape", fun=clear)
+screen.onkey(key="c", fun=clear)
+
+
+clear()
+
+
 screen.exitonclick()
